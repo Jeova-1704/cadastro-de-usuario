85a6978fd6c92d06ae7e974397b17edb2b148661
Jeova BL <jeovab115@gmail.com>
2024-01-10 11:59:18 -0300

    fix<erro de serialização> corrigido o erro de serialização

--- src/main/java/br/com/aprendendospring/projeto_spring/DTO/UserDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.aprendendospring.projeto_spring.DTO;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record UserDTO(@NotBlank @NotNull String nome, @NotBlank @NotNull Integer idade) {
-
-}
