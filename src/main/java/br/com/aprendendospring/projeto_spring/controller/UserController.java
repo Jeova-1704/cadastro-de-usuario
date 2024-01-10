ca56edcec608a5435b66c31d33a1ea74cd722ddc
Jeova BL <jeovab115@gmail.com>
2024-01-10 11:34:05 -0300

    feat<UserService>: criação da camada de serviços

--- src/main/java/br/com/aprendendospring/projeto_spring/controller/UserController.java
+++ src/main/java/br/com/aprendendospring/projeto_spring/controller/UserController.java
@@ -1,7 +1,9 @@
 package br.com.aprendendospring.projeto_spring.controller;
 
+import br.com.aprendendospring.projeto_spring.DTO.UserDTO;
 import br.com.aprendendospring.projeto_spring.domain.user.UserModel;
-import br.com.aprendendospring.projeto_spring.repository.UserRepository;
+import br.com.aprendendospring.projeto_spring.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,74 +19,37 @@ import java.util.Optional;
 public class UserController {
 
     @Autowired
-    private UserRepository repository;
+    private UserService service;
 
-    @GetMapping("/todos")//Lista todos os usuarios do meu banco de dados
+    @GetMapping("/todos")
     public ResponseEntity<List<UserModel>> bucarUsuarios() {
-        List<UserModel> usuarios = repository.findAll();
+        List<UserModel> usuarios = service.listarTodos();
         return ResponseEntity.ok().body(usuarios);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserModel> buscarUsuarioId(@PathVariable Long id) {
-        Optional<UserModel> usuarioOptional = repository.findById(id);
-        UserModel usuario = usuarioOptional.get();
+        UserModel usuario = service.buscarPorId(id);
         return ResponseEntity.ok().body(usuario);
     }
 
-    @GetMapping("/buscarid")
-    public ResponseEntity<UserModel> buscarUsuarioPorId(@RequestBody Map<String, Long> requestBody) {
-        Long id = requestBody.get("id");
-
-        Optional<UserModel> usuarioOptional = repository.findById(id);
-
-        if (usuarioOptional.isPresent()) {
-            UserModel usuario = usuarioOptional.get();
-            return ResponseEntity.ok().body(usuario);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
     @PostMapping("/salvar")
-    public ResponseEntity<UserModel> salvar(@RequestBody UserModel userModel ){
-        UserModel user = repository.save(userModel);
+    public ResponseEntity<UserModel> salvar(@RequestBody @Valid UserDTO userDTO ){
+        UserModel user = service.salvar(userDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
 
-    @DeleteMapping("/deletar")
-    public ResponseEntity<String> delete(@RequestBody Map<String, Long> requestBody) {
-        Long id = requestBody.get("id");
-        UserModel usuario = repository.getReferenceById(id);
-        repository.delete(usuario);
-
-        return ResponseEntity.ok().body("Usuário deletado com sucesso");
-    }
-
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
-        UserModel usuario = repository.getReferenceById(id);
-        repository.delete(usuario);
-        return ResponseEntity.ok().body("Usuário deletado com sucesso");
+        UserModel usuario = service.buscarPorId(id);
+        String msg = service.deletarPorId(id);
+        return ResponseEntity.ok().body(msg);
     }
-
-
-    @PutMapping("/atualizar")
-    public ResponseEntity<?> atualizar(@RequestBody UserModel userModel ){
-
-        if(userModel.getId() == null) {
-            return ResponseEntity.badRequest().body("Id não informado para atualização. Por favor, fornecer id não nulo e válido.");
-        }
-
-        UserModel user = repository.saveAndFlush(userModel);
-        return ResponseEntity.ok().body(user);
-    }
-
 
     @GetMapping("buscarnome/{nome}")
     public ResponseEntity<List<UserModel>> buscarPorNome(@PathVariable String nome){
-        List<UserModel> listaUsuarios = repository.buscarPorNome(nome.trim().toUpperCase());
+        List<UserModel> listaUsuarios = service.buscarPorNome(nome);
         return ResponseEntity.ok().body(listaUsuarios);
     }
 }
